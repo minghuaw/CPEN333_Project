48f7023875feb7f699642c89fa31adc17b8613a2
minghuaw <michael.wu1107@gmail.com>
2017-11-30 15:15:19 -0800

    completed Truck.h

--- header/Truck.h
+++ header/Truck.h
@@ -6,15 +6,36 @@
 #define CPEN333_PROJECT_TRUCK_H
 
 #include <string>
+#include <iostream>
 #include <vector>
+#include "Item.h"
+#include "TruckQueue.h"
 
 class Truck{
 private:
-    std::string ID;
-    double capacity;
-    
-
+    std::string ID;                     // truck ID
+    double capacity;                    // loading capacity of the truck
+    std::vector<Item> itemVector;       // vector storing all items on the truck
+    TruckQueue truckQueue;              // communication queue with warehouseComputer
 public:
+    /**
+     * @brief wait until semaphore notifies it's available
+     * @return true if successful, false if not successful
+     */
+    bool dock(){}
 
+    /**
+     * @brief notify computer about truck arrival using truckQueue
+     * Cast ID, capacity, and action (arrive) into TruckInfo 
+     * and push TruckInfo into TruckQueue
+     */
+    void notifyArrival(){}
+
+    /**
+     * @brief notify computer about truck leaving using truckQueue
+     * Cast ID, capacity, and action (departure) into TruckInfo 
+     * and push TruckInfo into TruckQueue
+     */
+    void notiryLeaving(){}
 };
 #endif //CPEN333_PROJECT_TRUCK_H
